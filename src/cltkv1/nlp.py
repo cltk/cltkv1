cc525b6f6b69944d96e6911f757ed3f4a785a57c
Kyle P. Johnson <kyle.p.johnson>
2019-11-02 18:05:04 -0700

    use examnple texts for test

--- src/cltkv1/nlp.py
+++ src/cltkv1/nlp.py
@@ -5,6 +5,7 @@ from typing import List
 
 from cltkv1.tokenizers.sentence import DefaultSplitter
 from cltkv1.tokenizers.word import Tokenizer, dummy_get_token
+from cltkv1.utils import example_texts
 from cltkv1.utils.data_types import Doc
 from cltkv1.utils.pipelines import DefaultPipeline, LatinPipeline
 
@@ -17,14 +18,13 @@ class NLP:
     >>> cltk_nlp = NLP(language='greek')
     >>> cltk_nlp.language
     'greek'
-    >>> john_damascus_corinth = "Τοῦτο εἰπὼν, ᾐνίξατο αἰτίους ὄντας τοῦ τὰ ἐλάσσονα λαμβάνειν, καὶ κυρίους, εἰ βούλοιντο, τοῦ τὰ μείζονα. Ἔστι δὲ πολὺ μείζων ἡ ἀγάπη πάντων τῶν χαρισμάτων."
-    >>> john_text_analyzed = cltk_nlp.analyze(john_damascus_corinth)
-    >>> john_text_analyzed.language
+    >>> greek_text_analyzed = cltk_nlp.analyze(example_texts.GREEK)
+    >>> greek_text_analyzed.language
     'greek'
-    >>> john_text_analyzed.indices_sentences
-    [[103, 104], [154, 155]]
-    >>> john_text_analyzed.indices_tokens[0:3]
-    [[0, 5], [6, 11], [13, 20]]
+    >>> greek_text_analyzed.indices_sentences
+    [[155, 156], [204, 205], [349, 350], [641, 642], [1095, 1096], [1352, 1353], [1369, 1370], [1477, 1478], [1877, 1878]]
+    >>> greek_text_analyzed.indices_tokens[0:3]
+    [[0, 3], [4, 7], [8, 13]]
     """
 
     def __init__(self, language: str, custom_pipeline: List[str] = None) -> None:
@@ -59,16 +59,15 @@ class NLP:
         >>> cltk_nlp = NLP(language='latin')
         >>> isinstance(cltk_nlp, NLP)
         True
-        >>> galileo_sidereus = "Preclarum sane atque humanitatis plenum eorum fuit institutum, qui excellentium virtute virorum res preclare gestas ab invidia tutari, eorumque immortalitate digna nomina ab oblivione atque interitu vindicare, conati sunt. Hinc ad memoriam posteritatis prodite imagines, vel marmore insculpte, vel ex ere ficte; hinc posite statue, tam pedestres, quam equestres; hinc columnarum atque pyramidum, ut inquit ille, sumptus ad sidera ducti; hinc denique urbes edificate, eorumque insignite nominibus, quos grata posteritas eternitati commendandos existimavit. Eiusmodi est enim humane mentis conditio, ut nisi assiduis rerum simulacris in eam extrinsecus irrumpentibus pulsetur, omnis ex illa recordatio facile effluat."
-        >>> analyzed_text = cltk_nlp.analyze(galileo_sidereus)
+        >>> analyzed_text = cltk_nlp.analyze(example_texts.LATIN)
         >>> analyzed_text.language
         'latin'
         >>> analyzed_text.indices_sentences
-        [[221, 222], [554, 555], [714, 715]]
+        [[144, 145], [201, 202], [274, 275], [572, 573], [768, 769], [985, 986], [1122, 1123], [1269, 1270]]
         >>> analyzed_text.indices_tokens[0:3]
-        [[0, 9], [10, 14], [15, 20]]
+        [[0, 6], [7, 10], [11, 16]]
         >>> analyzed_text.tokens[0]
-        Word(index_char_start=0, index_char_stop=9, index_token=0, index_sentence=None, string='Preclarum', pos=None, scansion=None)
+        Word(index_char_start=0, index_char_stop=6, index_token=0, index_sentence=None, string='Gallia', pos=None, scansion=None)
         """
 
         sentence_splitter = DefaultSplitter()
